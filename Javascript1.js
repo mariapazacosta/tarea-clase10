d414daaf657bcc10df094dd80761f2e52ad3e006
mariapazacosta <130701508+mariapazacosta@users.noreply.github.com>
2023-05-31 14:10:51 -0300

    Add files via upload

---
+++ Javascript1.js
@@ -0,0 +1,13 @@
+let nombre="Maria" ;//string
+let apellido="Acosta" //string
+
+console.log (nombre+apellido)
+
+var numero=10; //number
+var number=35; //number
+
+var suma=
+console.log (numero+number)
+
+var resultado= suma+25;
+console.log (suma+25);
